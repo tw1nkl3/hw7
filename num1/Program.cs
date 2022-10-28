c9b1dc578bf59c7f8069915173fc3d6f30a897bb
Ivan <grabovivan@gmailcom>
2022-10-28 11:33:52 +0300

    num1 added

---
+++ num1/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9 */
+
+Console.WriteLine("Введите m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[m, n];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        Console.Write(array[i,j] + ";");
+    }
+    Console.WriteLine();
+}
+
+
